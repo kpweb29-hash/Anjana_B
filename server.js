6438d59f6ee6c243d335ff434b91c08236dfe468
VANSHITAWEBEARL <vanshita.webearl@gmail.com>
2025-12-19 23:31:59 +0530

    Update emailController to use SendGrid API instead of nodemailer SMTP

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
